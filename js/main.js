07fe94b61dbb52fb84605c2244f490867f8bdc96
juancar1 <juancarpliego@gmail.com>
2018-09-27 14:00:57 +0200

    zona administradores

--- js/main.js
+++ js/main.js
@@ -25,10 +25,12 @@ $(document).ready(function () {
       })
 
 
-
-      
+      //coger el valor del select del trabajador para crear usuario
+      // $('.crear_registro_admin').on('submit', function (e) {
+      //   e.preventDefault();
+      //   console.log("click");
     
-  
+      // });
 
   
 
